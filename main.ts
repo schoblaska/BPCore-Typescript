2c6f648663f7646928e93c14a1a228fc43cd8b2a
Joey Schoblaska <joey.schoblaska@gmail.com>
2023-11-20 08:44:32 -0600

    prettier

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 import "./bpcore.ts";
 
-
 print("hello, world!", 1, 1);
 print("version: " + _BP_VERSION, 1, 3);
 
 while (true) {
-    // main loop, we need to call clear/display at least once for stuff to show
-    // up.
-    clear();
-    display();
+  // main loop, we need to call clear/display at least once for stuff to show
+  // up.
+  clear();
+  display();
 }
